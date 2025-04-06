cf0c497519b2e8fcbbe94d5bf849d8501d3f2794
Hoffie2025 <hoffmanedmundjacob@gmail.com>
2025-04-05 22:55:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AypaConnect from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AypaConnect />
+  </React.StrictMode>
+);
